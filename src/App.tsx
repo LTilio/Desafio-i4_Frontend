1192244803cf04bc1aa9bc2828be89c3edd8e6d8
LTilio <Leandro.tilio@gmail.com>
2024-07-25 12:37:08 -0300

    Feat: footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar/NavBar";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div id="root">
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
